1ba05bea8e8e456c98a241188c3febbe9aeafeb0
ReutGoldberg <reutgoldberg@gmail.com>
2022-08-31 18:10:20 +0300

    cosmetics mostly

--- volunteer_scheduler_app/src/components/AdminsList.tsx
+++ volunteer_scheduler_app/src/components/AdminsList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import List from "@mui/material/List";
-import {getAdminsList} from "../utils/DataAccessLayer";
+import { getAdminsList } from "../utils/DataAccessLayer";
 import CircularProgress from "@mui/material/CircularProgress";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
@@ -15,73 +15,72 @@ interface AdminListProps {
   curAdminList: any;
 }
 
-
-export const AdminsList: React.FC<AdminListProps> = ({curAdminList}) => {
+export const AdminsList: React.FC<AdminListProps> = ({ curAdminList }) => {
   console.log(`This is the prop: ${curAdminList}`);
-  
+
   const [admins, setAdmins] = React.useState(curAdminList);
 
-  const [isPending, setIsPending]  = React.useState(true);
+  const [isPending, setIsPending] = React.useState(true);
   const boxRef = useRef(null);
   const listRef = useRef(null);
 
-  const {user} = React.useContext(UserObjectContext);
+  const { user } = React.useContext(UserObjectContext);
 
   //set admin list one on initial load
-  useEffect(()=>{
+  useEffect(() => {
     const userToken = user.token;
-    getAdminsList(userToken)
-      .then(data => {
-        console.log("Below are the admins") //todo: remove when done testing
-        console.log(data);
-        setAdmins(data);
-        setIsPending(false);
-      })
+    getAdminsList(userToken).then((data) => {
+      console.log("Below are the admins"); //todo: remove when done testing
+      console.log(data);
+      setAdmins(data);
+      setIsPending(false);
+    });
   }, []);
 
   //will this result in an infinite loop? - no
-  useEffect(()=> {
-    if(curAdminList.length > 0){
-      console.log("CurAdminList is greater than 0")
-      setAdmins(curAdminList)
+  useEffect(() => {
+    if (curAdminList.length > 0) {
+      console.log("CurAdminList is greater than 0");
+      setAdmins(curAdminList);
       setIsPending(false);
-    }},[curAdminList]);
-    
+    }
+  }, [curAdminList]);
 
   return (
-    <Box id="AdminListTabId" ref={boxRef} >
-      <List  id="adminsList" ref={listRef} style={{maxHeight:200, overflow:'auto'}}>
-      {isPending && <CircularProgress color="primary" size={100}/>
-      }
-      {admins && (
-        //@ts-ignore
-        admins.map((adminUser, index) => (
-                    <ListItem disablePadding key={index}
-                      sx={{
-                        color: "primary.dark",
-                        display: "inline",
-                        fontSize: 17,
-                        fontWeight: "bold",
-                        gap: 3,
-                        paddingTop: 1,
-                        paddingBottom: 1,
-                      }}
-                    >
-                      <ListItemButton>
-                        <ListItemAvatar>
-                          <Avatar variant="rounded">
-                            <AccountCircle/>
-                          </Avatar>
-                        </ListItemAvatar>
-                          <ListItemText primary={adminUser.email} />
-                      </ListItemButton>
-                    </ListItem>
-          )))
-      }
+    <Box id="AdminListTabId" ref={boxRef}>
+      <List
+        id="adminsList"
+        ref={listRef}
+        style={{ maxHeight: 200, overflow: "auto" }}
+      >
+        {isPending && <CircularProgress color="primary" size={100} />}
+        {admins &&
+          //@ts-ignore
+          admins.map((adminUser, index) => (
+            <ListItem
+              disablePadding
+              key={index}
+              sx={{
+                color: "primary.dark",
+                display: "inline",
+                fontSize: 17,
+                fontWeight: "bold",
+                gap: 3,
+                paddingTop: 1,
+                paddingBottom: 1,
+              }}
+            >
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar variant="rounded">
+                    <AccountCircle />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={adminUser.email} />
+              </ListItemButton>
+            </ListItem>
+          ))}
       </List>
     </Box>
   );
-}
-
-
-
+};
