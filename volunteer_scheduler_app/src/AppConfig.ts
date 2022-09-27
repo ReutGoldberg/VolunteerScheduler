d7b9f64463134af49ab2e9b59536cb1cfe0777ef
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2022-09-27 16:32:26 +0300

    trying to force remote server url

--- volunteer_scheduler_app/src/AppConfig.ts
+++ volunteer_scheduler_app/src/AppConfig.ts
@@ -2,7 +2,7 @@
  export const AppConfig = {
     client_id: "83163129776-q90s185nilupint4nb1bp0gsi0fb61vs.apps.googleusercontent.com",
     token_iss_1: "https://accounts.google.com",
-    server_url: process.env.API_URL || "http://localhost:5001",
+    server_url: process.env.API_URL,
     sessionStorageContextKey: "VolunteerSchedUserObj",
     IS_SHOW_FAKE: false
    };
