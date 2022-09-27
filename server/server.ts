0e54eeea8f207343a0450b8e06c44bf4966d7fd4
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2022-09-27 15:47:19 +0300

    trying to fix remote server

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,10 @@
-//import express, {Express, Request, Response} from "express";
-const express =  require("express");
-import {Request, Response} from "express";
+import express, {Express, Request, Response} from "express";
+
 import {config} from "./config";
 import helmet from "helmet";
 //const config = require('./config')
 
-const app: any = express();
+const app: Express = express();
 app.use(helmet());
 var cors = require('cors');
 app.use(cors({origin: config.client_app.localhost})) 
